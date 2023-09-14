94b85e50320f3c8da5de1dbdc5e8667ac29e904f
Zabdiel Rojas <zabdielrojas5@gmail.com>
2023-09-14 18:27:22 +0200

    Feat NewNoteForm añade formulario de creacion de notas

--- AppDeNotas/src/App.jsx
+++ AppDeNotas/src/App.jsx
@@ -1,10 +1,16 @@
 import "./index.css";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import { NotePage } from "./pages/NotePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import Modal from "./components/Modal";
+import NewNoteForm from "./components/NewNoteForm";
+import Button from "./components/Button";
+
 function App() {
+const [showModal, setShowModal] = useState(false)
   return (
     <>
       <Header></Header>
@@ -13,7 +19,7 @@ function App() {
         <Route path="/notes/:id" element={<NotePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      <footer>Por Zabdiel y Ari</footer>
+      <footer>{showModal&&<Modal setShowModal={setShowModal}><NewNoteForm setShowModal={setShowModal} /></Modal>}<Button text={"Nueva nota"} handleOnClick={()=>{setShowModal(true)}} /></footer>
     </>
   );
 }
---
+++ AppDeNotas/src/components/NewNoteForm/index.jsx
@@ -0,0 +1,122 @@
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTokenContext } from "../../contexts/TokenContext";
+
+const NewNoteForm = ({ setShowModal }) => {
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteText, setNoteText] = useState("");
+  const [noteCategory, setNoteCategory] = useState("Otras");
+  const [uploadImageText, setUploadImageText] = useState("¡Sube una foto!")
+  const { token } = useTokenContext();
+
+  const noteImageRef = useRef();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+
+      const noteImageData = noteImageRef.current?.files;
+
+      const formData = new FormData();
+
+      formData.set("title", noteTitle);
+      formData.set("text", noteText);
+      formData.set("category", noteCategory);
+
+      if (noteImageData.length) {
+        for (const image of noteImageData) {
+          formData.set("image", image);
+        }
+      }
+
+      const res = await fetch(`http://localhost:8000/notes`, {
+        method: "POST",
+        headers: { Authorization: token },
+        body: formData,
+      });
+
+      const body = await res.json();
+      if (!res.ok) {
+        throw new Error(body.message);
+      }
+      setNoteTitle("");
+      setNoteText("");
+      setNoteCategory("");
+      setShowModal(false);
+      navigate(`/notes/${body.data.id}`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <form className="new-note-form" onSubmit={handleSubmit}>
+      <label hidden htmlFor="new-note-title" className="new-note-title">
+        Título
+      </label>
+      <input
+        id="new-note-title"
+        type="text"
+        value={noteTitle}
+        onChange={(event) => {
+          setNoteTitle(event.target.value);
+        }}
+        placeholder="Título"
+      />
+      <label hidden htmlFor="new-note-text" className="new-note-text">
+        Texto
+      </label>
+      <input
+        id="new-note-text"
+        type="text"
+        value={noteText}
+        onChange={(event) => {
+          setNoteText(event.target.value);
+        }}
+        placeholder="¿En que piensas?"
+      />
+
+      <label className="new-note-image" htmlFor="new-note-image">
+        <p>
+          {uploadImageText.length <= 30
+            ? uploadImageText
+            : uploadImageText.substring(0, 30) + "..."}
+        </p>
+      </label>
+
+      <input
+        hidden
+        id="new-note-image"
+        onChange={(event) => {
+          setUploadImageText(event.target.value);
+        }}
+        type="file"
+        accept="image/*"
+        ref={noteImageRef}
+      />
+
+      <label className="new-note-category" htmlFor="new-note-category">
+        Categoría
+      </label>
+
+      <select
+        value={noteCategory}
+        onChange={(event) => {
+          setNoteCategory(event.target.value);
+        }}
+        id="new-note-category"
+      >
+        <option value={"Otras"}>Otras</option>
+        <option value={"importantes"}>Importantes</option>
+        <option value={"emergencia"}>Emergencia</option>
+        <option value={"citas"}>Citas</option>
+      </select>
+
+      <button>Crear</button>
+    </form>
+  );
+};
+
+export default NewNoteForm;
