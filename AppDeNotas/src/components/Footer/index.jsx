53160b8e38df84cef935d6f26c087da58ec972e4
arianarumbos <arianarumbos@gamil.com>
2023-09-27 19:03:31 +0200

    feat HomePage añade selector de categorias

--- AppDeNotas/src/components/Footer/index.jsx
+++ AppDeNotas/src/components/Footer/index.jsx
@@ -1,22 +1,32 @@
-import "./style.css"
-import Modal from "../Modal/index.jsx"
-import NewNoteForm from "../NewNoteForm"
-import { useState } from "react"
-import Button from "../Button"
+import "./style.css";
+import Modal from "../Modal/index.jsx";
+import NewNoteForm from "../NewNoteForm";
+import { useState } from "react";
+import Button from "../Button";
+import { useTokenContext } from "../../contexts/TokenContext";
+import { toast } from "react-toastify";
 
-export const Footer = ()=>{
-    const [showModal, setShowModal] = useState(false)
-    return(
-<footer className="main-footer">
-{showModal && (
-  <Modal setShowModal={setShowModal}>
-    <NewNoteForm setShowModal={setShowModal} />
-  </Modal>
-)}
-<Button
-  text={"Nueva nota"}
-  handleOnClick={() => {
-    setShowModal(true);
-  }}
-/>
-</footer>)}
+export const Footer = () => {
+  const { token } = useTokenContext();
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <footer className="main-footer">
+      {showModal && (
+        <Modal setShowModal={setShowModal}>
+          <NewNoteForm setShowModal={setShowModal} />
+        </Modal>
+      )}
+      <Button
+        text={"Nueva nota"}
+        handleOnClick={() => {
+          if (token) {
+            setShowModal(true);
+          } else {
+            toast.warning("¡Regístrate para crear una Nota!");
+          }
+        }}
+      />
+    </footer>
+  );
+};
--- AppDeNotas/src/pages/HomePage/index.jsx
+++ AppDeNotas/src/pages/HomePage/index.jsx
@@ -1,18 +1,44 @@
-import "./style.css"
+import "./style.css";
 import { ListNotes } from "../../components/ListNotes";
-import {Button} from "../../components/Button"
+import { Button } from "../../components/Button";
 import { useGetNotes } from "../../hooks/useGetNotes";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTokenContext } from "../../contexts/TokenContext";
 export const HomePage = () => {
-  const {token} = useTokenContext()
- const {notes, error, loading} = useGetNotes()
- if(!token){return}<></> // To do: rellenar con algo.
-  return (<>
-    {loading&&<p>Cargando</p>}
-    {!loading&&<main>
-      {notes.length > 0 ? (<ListNotes notes={notes} />) : (<p>No hay notas</p>)}
-    </main>}
-    </> );
+  const { token } = useTokenContext();
+  const { notes, error, loading, setSearchParams } = useGetNotes();
+  const [category, setCategory] = useState("Todas");
+  if (!token) {
+    return;
+  }
+  <></>; // To do: rellenar con algo.
+  return (
+    <>
+      {loading && <p>Cargando</p>}
+      {!loading && (
+        <main>
+          <header>
+            <div className="new-note-category">
+              <select
+                id="new-note-category"
+                value={category}
+                onChange={(event) => {
+                  setCategory(event.target.value);
+                  setSearchParams({ category: event.target.value });
+                }}
+              >
+                <option value={""}>Todas</option>
+                <option value={"Otras"}>Otras</option>
+                <option value={"importantes"}>Importantes</option>
+                <option value={"emergencia"}>Emergencia</option>
+                <option value={"citas"}>Citas</option>
+              </select>
+            </div>
+          </header>
+          {notes.length > 0 ? <ListNotes notes={notes} /> : <p>No hay notas</p>}
+        </main>
+      )}
+    </>
+  );
 };
 export default HomePage;
